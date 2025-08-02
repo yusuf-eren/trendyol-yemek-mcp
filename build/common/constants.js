5e5079fa0523036d05a408fe856a871a580e2283
Yusuf Eren <erenyusuf170@gmail.com>
2025-08-02 23:33:37 +0300

    s

--- build/common/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_RESTAURANT_URL = exports.API_BASE_URL = void 0;
-exports.API_BASE_URL = 'https://api.tgoapis.com/web-discovery-apidiscovery-santral';
-exports.API_RESTAURANT_URL = 'https://api.tgoapis.com/web-restaurant-apirestaurant-santral';
